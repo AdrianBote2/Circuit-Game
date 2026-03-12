93987b6eb9037f05dd33590323a6ce4a47f700b7
Adrian Botello <adrianbotello1234@gmail.com>
2026-03-11 20:57:37 -0700

    Testing phaser

--- js/Index.js
+++ js/Index.js
@@ -1,35 +1,44 @@
 function startGame() {
     document.getElementById('home-screen').style.display = 'none';
     document.getElementById('play-screen').style.display  = 'flex';
-    //create a new scene 
-    let gameScene = new Phaser.Scene('Game');
+
     
     //set the configuration of the game 
     let config = {
         type: Phaser.CANVAS,
-        width: 550,
-        height: 400,
         canvas: document.getElementById('game-canvas'),
         parent: 'play-screen',
-        scale:{
-            mode: Phaser.Scale.FIT,
-            
-            autoCenter: Phaser.Scale.CENTER_BOTH
-
-            autoRound: true
+        scale: {
+        mode: Phaser.Scale.RESIZE, 
+        width: '100%',
+        height: '100%'
         },
-
-        render: {
-            pixelArt: true,
-            antialias: false
-        },
-        scene: gameScene
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
             
     };
 
     // create a new game, pass tje cpmfogiratopm
     let game = new Phaser.Game(config);
+
+    function preload ()
+    {
+        this.load.image('sky', 'js/sky.png');
     }
+
+    function create ()
+    {
+        this.add.image(400, 300, 'sky');
+    }
+
+    function update ()
+    {
+    }
+} 
+
 function homeScreen() {
     document.getElementById('howToPlay').style.display = 'none';
     document.getElementById('home-screen').style.display = 'flex';
