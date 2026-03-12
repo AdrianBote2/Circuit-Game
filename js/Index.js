9b81ac39efff1b7fbdbd91d0b5d0a9e10543d938
AdrianBote2 <138329587+AdrianBote2@users.noreply.github.com>
2026-03-12 14:17:31 -0700

    upadated format

--- js/Index.js
+++ js/Index.js
@@ -1,46 +1,12 @@
 function startGame() {
     document.getElementById('home-screen').style.display = 'none';
     document.getElementById('play-screen').style.display  = 'flex';
-
-    
-    //set the configuration of the game 
-    let config = {
-        type: Phaser.CANVAS,
-        canvas: document.getElementById('game-canvas'),
-        parent: 'play-screen',
-        scale: {
-        mode: Phaser.Scale.RESIZE, 
-        width: '100%',
-        height: '100%'
-        },
-        scene: {
-            preload: preload,
-            create: create,
-            update: update
-        }
-            
-    };
-
-    // create a new game, pass tje cpmfogiratopm
-    let game = new Phaser.Game(config);
-
-    function preload ()
-    {
-        this.load.image('sky', 'js/sky.png');
-    }
-
-    function create ()
-    {
-        this.add.image(400, 300, 'sky');
-    }
-
-    function update ()
-    {
-    }
-} 
+    startTimer();
+}
 
 function homeScreen() {
     document.getElementById('howToPlay').style.display = 'none';
+    document.getElementById('play-screen').style.display = 'none';
     document.getElementById('home-screen').style.display = 'flex';
 }
 
@@ -48,3 +14,41 @@ function howToPlay() {
     document.getElementById('home-screen').style.display = 'none';
     document.getElementById('howToPlay').style.display = 'flex';
 }
+//Drag and 
+function dragStartHandler(event){
+    event.dataTransfer.setData("text/plain", event.target.id);
+    event.target.style.opacity = '0.4';
+}
+
+
+
+//Timer function
+
+/* let timeLeft = 30;
+let timerInterval = null;
+
+function startTimer() {
+    setInterval(function() {
+        timeLeft--;
+        document.getElementById('timer').innerText = timeLeft;
+        if (timeLeft== -1){
+            clearInterval(timerInterval);
+            gameOver();
+        }
+ 
+    }, 1000);
+}
+function gameOver(){
+    clearInterval(timerInterval);
+    timeLeft = 30;
+    document.getElementById('timer').innerText = 30;
+    alert('Boom');
+    homeScreen();
+} */
+
+const circuit = {
+    voltage: 9,
+    current: 3,
+    resistance: 3
+};
+
